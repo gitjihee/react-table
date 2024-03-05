8d71dfc8ad99051561959dd07d459123a0fea39b
choi.jihee <choi_jihee@radcns.com>
2024-03-05 14:59:17 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import TableTr from './components/TableTr.jsx';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  // 서버에 데이터 요청
+  useEffect(()=>{    
+    fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
+      .then(res=>res.json())
+      .then(res=>{
+        setData(res)
+      });
+  }, [])
+
   return (
     <div className="App">
-      
+      <table>
+        <tbody>
+          {
+            data.map((el,i)=> {
+              return (
+                <TableTr key={`tr-${i}`} item={el}/>
+              )
+            })
+          }          
+        </tbody>        
+      </table>
     </div>
   );
 }
---
+++ src/components/TableTr.jsx
@@ -0,0 +1,9 @@
+const TableTr = (props) => {
+    console.log(props);
+    return (
+        <tr>
+            <td></td>
+        </tr>
+    )
+}
+export default TableTr;
